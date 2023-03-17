8994afbecd9a9c2cefcb9bb024f965185b3cfaa8
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-03-16 21:12:57 -0300

    if fail

--- script.js
+++ script.js
@@ -9,26 +9,27 @@ var cor_3 = document.getElementById("cor3");
 
 /*console.log(cor0);*/
 
-cor_0.addEventListener('mousedown', (event) => {
-    document.getElementById("cor0").style.background = 'radial-gradient(circle, rgb(187, 255, 187), rgb(0, 204, 0))';
-})
+
 cor_0.addEventListener('mouseup', (event) => {
     document.getElementById("cor0").style.background = 'linear-gradient(270deg, green, rgb(0, 204, 0))';
 })
 cor_1.addEventListener('mousedown', (event) => {
     document.getElementById("cor1").style.background = 'radial-gradient(circle, rgb(252, 174, 174), red)';
+    teste = 1;
 })
 cor_1.addEventListener('mouseup', (event) => {
     document.getElementById("cor1").style.background = 'linear-gradient(270deg, rgb(140, 1, 1), red)';
 })
 cor_2.addEventListener('mousedown', (event) => {
     document.getElementById("cor2").style.background = 'radial-gradient(circle, rgb(255, 255, 197), yellow)';
+    teste = 2;
 })
 cor_2.addEventListener('mouseup', (event) => {
     document.getElementById("cor2").style.background = 'linear-gradient(270deg, rgb(149, 149, 0), rgb(235, 235, 0)';
 })
 cor_3.addEventListener('mousedown', (event) => {
     document.getElementById("cor3").style.background='radial-gradient(circle, rgb(205, 205, 252), rgb(13, 0, 255))';  
+    teste = 3;
 })
 cor_3.addEventListener('mouseup', (event) => {
     document.getElementById("cor3").style.background = 'linear-gradient(270deg, rgb(0, 0, 122), rgb(13, 0, 255))';
@@ -122,9 +123,21 @@ function jogo(){
     /*alert(jogada[50])*/ //teste do vetor
     for (var i = 0; i < 4; i++) {
         liga_botao_bot(i);
-        for (var j = 0; j <= 4; j++) {
-            
-        }
+        /*cont = 0;
+        if(cont <= i && jogada[i] == teste){
+            cont++;
+        }*/
+        //console.log("OK"+1);
+        for (var j = 0; j < 4; j++) {
+            if(cor_0.addEventListener('mousedown', (event) => {
+                document.getElementById("cor0").style.background = 'radial-gradient(circle, rgb(187, 255, 187), rgb(0, 204, 0))';
+                teste = 0;
+                console.log(teste);
+                return true;
+            })){
+                console.log("OK"+1);
+            }
+         }
         //cont = 0;
         /*while (cont <= i) {
             //if (errou == -1){
